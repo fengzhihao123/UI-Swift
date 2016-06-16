1fefca6a1ba25a32b96921725bea65ae427938ce
fengzhihao123 <1374795493@qq.com>
2016-06-16 09:33:35 +0800

    add second progress

---
+++ SecondSwift/SecondSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondSwift
+//
+//  Created by 冯志浩 on 16/6/16.
+//  Copyright © 2016年 FZH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("123")
+    }
+
+}
+
