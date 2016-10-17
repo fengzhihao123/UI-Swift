55111bbd8bed36e883c6ccf37231f5d61bfdcd33
fengzhihao123 <1374795493@qq.com>
2016-10-17 13:39:05 +0800

    添加textfield自适应键盘高度

--- 02-UITextView/02-UITextView/ViewController.swift
+++ 02-UITextView/02-UITextView/ViewController.swift
@@ -9,27 +9,76 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let textField = UITextField()
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupButton()
         setupTextfield()
         setupTextView()
+        setupTextfieldNotification()
+    }
+    
+    func setupButton() -> Void {
+        let secureBtn = UIButton()
+        secureBtn.frame = CGRect(x: 0, y: 20, width: 100, height: 50)
+        secureBtn.tag = 1001
+        secureBtn.setTitle("secure", for: .normal)
+        secureBtn.addTarget(self, action: #selector(btnDidTouch(btn:)), for: .touchUpInside)
+        secureBtn.backgroundColor = UIColor.green
+        secureBtn.setTitleColor(UIColor.black, for: .normal)
+        view.addSubview(secureBtn)
+        
+        let cornerBtn = UIButton()
+        cornerBtn.frame = CGRect(x: 120, y: 20, width: 100, height: 50)
+        cornerBtn.tag = 1002
+        cornerBtn.setTitle("corner", for: .normal)
+        cornerBtn.backgroundColor = UIColor.green
+        cornerBtn.addTarget(self, action: #selector(btnDidTouch(btn:)), for: .touchUpInside)
+        cornerBtn.setTitleColor(UIColor.black, for: .normal)
+        view.addSubview(cornerBtn)
     }
 
     func setupTextfield() -> Void {
-        let textField = UITextField()
         textField.backgroundColor = UIColor.brown
-        textField.frame = CGRect(x: 0, y: 20, width: 300, height: 50)
+        textField.frame = CGRect(x: 0, y: 500, width: 300, height: 50)
         textField.placeholder = "textfield..."
         view.addSubview(textField)
+    }
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    func setupTextfieldNotification() -> Void {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyWillShow), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyWillHide), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func keyWillShow(notification: Notification) -> Void {
+        let keyboardF: CGRect = ((notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue)!
+        UIView.animate(withDuration: 0.25) {
+            self.textField.transform = CGAffineTransform.init(translationX: 0, y: keyboardF.origin.y - self.textField.frame.size.height - self.textField.frame.origin.y)
+        }
+    }
+    
+    func keyWillHide(notification: Notification) -> Void {
+        UIView.animate(withDuration: 0.25) {
+            self.textField.transform = .identity
+        }
     }
     
     func setupTextView() -> Void {
         let textView = FZHTextView()
         textView.backgroundColor = UIColor.gray
-        textView.frame = CGRect(x: 0, y: 100, width: 300, height: 200)
+        textView.frame = CGRect(x: 0, y: 300, width: 300, height: 200)
         textView.placeholder = "textview..."
         view.addSubview(textView)
+    }
+    
+    func btnDidTouch(btn: UIButton) -> Void {
+        if btn.tag == 1001{
+            textField.isSecureTextEntry = true
+        }else if btn.tag == 1002{
+            textField.layer.cornerRadius =  5
+        }
     }
 }
 
